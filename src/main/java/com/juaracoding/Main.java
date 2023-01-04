a183ee785a488d28a89e82469f02f06c3f3e90ca
maf-muhammad-abdul-fajar <muhammadabdulfajar9@gmail.com>
2023-01-04 20:41:00 +0700

    DISINI ADALAH HISTORY PROSES COMMIT NYA. SETIAP PROSES COMMIT DARI DEVEPLOMENT YANG SUDAH DI KERJAKAN, NANTI AKAN MUNCUL INFORMASI.JADI HARUS DIISI DENGAN CUKUP JELAS INFORMASI DARI SETIAP PROSES COMMIT KE GITHUB DARI IDE LOKAL

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -1,4 +1,6 @@
-package com.juaracoding;public class Main {
+package com.juaracoding;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
